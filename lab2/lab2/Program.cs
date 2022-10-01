d036847dc7ce77ad79966a6faa2dad8a9d6163c2
Dmitry <puchkovdmitrybor@gmail.com>
2022-10-01 18:13:48 +0700

    create repo for all criptography labs, create lab2 project. Add Rsa Key Generation

---
+++ lab2/lab2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+
+namespace lab2
+{
+   class Program
+   {
+      
+      static void Main(string[] args)
+      {
+         Int64 p, q;
+         var rsa = new Rsa();
+         Console.WriteLine("Input p:");
+         p = Int64.Parse(Console.ReadLine());
+         Console.WriteLine("Input q:");
+         q = Int64.Parse(Console.ReadLine());
+         try
+         {
+            rsa.GenKeys(p, q);
+         }
+         catch(Exception e)
+         {
+            Console.WriteLine(e);
+         }
+         
+
+      }
+   }
+}
---
+++ lab2/lab2/Rsa.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab2
+{
+   class Rsa
+   {
+      public Dictionary<string, List<Int64>> GenKeys(Int64 p, Int64 q)
+      {
+         // step 1
+         if (!isPrime(p) || !isPrime(q))
+            throw new Exception("Number(s) is not prime");
+
+         var keys = new Dictionary<string, List<Int64>>();
+
+         //step 2
+         Int64 n = p * q;
+
+         //step 3
+         Int64 lambda = Lcm(p - 1, q - 1);
+
+         //step 4, 5
+         Int64 e = 0;
+         Int64 d = 0;
+         bool flag = true;
+         Dictionary<string, Int64> eucklid_ifno;
+         for (Int64 i = 3; i<lambda && flag; i+=2)
+         {
+            eucklid_ifno = GcdEx(i, lambda);
+            if (isPrime(i) && eucklid_ifno["gcd"] == 1)
+            {
+               e = i;
+               d = eucklid_ifno["x"];
+               flag = false;
+            }
+         }
+         if (flag)
+            throw new Exception("Programm can't find e");
+         
+
+         keys.Add("publlic", new List<Int64>(){n, e});
+         keys.Add("private", new List<Int64>() { n, d });
+         return keys;
+      }
+
+      public Int64 Lcm(Int64 a, Int64 b)
+      {
+         var tmp = GcdEx(a, b)["gcd"];
+         return (a * b) / GcdEx(a, b)["gcd"];
+      }
+
+      public Dictionary<string, Int64> GcdEx(Int64 a, Int64 b)
+      {
+         var res = new Dictionary<string, Int64>();
+         if(a==0)
+            return new Dictionary<string, Int64>(){
+               { "gcd", b },
+               { "x", 0 },
+               { "y", 1 }
+            };
+
+         var tmpDict = GcdEx(b % a, a);
+         res.Add("x", tmpDict["y"] - (b / a) * tmpDict["x"]);
+         res.Add("y", tmpDict["x"]);
+         res.Add("gcd", tmpDict["gcd"]);
+         return res;
+      }
+
+      public static bool isPrime(Int64 a)
+      {
+         Int64 sqrta = (Int64)Math.Ceiling(Math.Sqrt(a));
+         for (Int64 i = 2; i <= sqrta; i++)
+            if (a % i == 0)
+               return false;
+         return true;
+      }
+   }
+}
