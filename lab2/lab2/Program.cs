6742229821a801302be1e10093dfa55ea8eff33a
Dmitry <puchkovdmitrybor@gmail.com>
2022-10-09 14:15:44 +0700

    add user cli interface

--- lab2/lab2/Program.cs
+++ lab2/lab2/Program.cs
@@ -1,15 +1,22 @@
 ﻿using System;
-
+using System.Collections.Generic;
 
 namespace lab2
 {
    class Program
    {
+      static void Help()
+      {
+         Console.WriteLine("Simple RSA usage:");
+         Console.WriteLine("gen - generate key pair");
+         Console.WriteLine("enc - encode number");
+         Console.WriteLine("dec - decode number");
+         Console.WriteLine("quit - exit from programm\n");
+      }
       
-      static void Main(string[] args)
+      static void GenKeys(Rsa rsa)
       {
          Int64 p, q;
-         var rsa = new Rsa();
          Console.WriteLine("Input p:");
          p = Int64.Parse(Console.ReadLine());
          Console.WriteLine("Input q:");
@@ -17,14 +24,81 @@ namespace lab2
          try
          {
             var keys = rsa.GenKeys(p, q);
-            Int64 M = 123456;
-            var C = rsa.Encode(M, keys["public"]);
-            var decM = rsa.Decode(C, keys["private"]);
-            Console.WriteLine(decM.ToString());
+            Console.WriteLine("Public Key:");
+            Console.WriteLine($"n: {keys["public"][0]}");
+            Console.WriteLine($"e: {keys["public"][1]}");
+            Console.WriteLine("\nPrivate Key:");
+            Console.WriteLine($"n: {keys["private"][0]}");
+            Console.WriteLine($"d: {keys["private"][1]}");
          }
-         catch(Exception e)
+         catch (Exception err)
          {
-            Console.WriteLine(e);
+            Console.WriteLine(err);
+         }
+      }
+
+      static void Encode(Rsa rsa)
+      {
+         Int64 n, e, M;
+         Console.WriteLine("Input Public key");
+         Console.WriteLine("Input n:");
+         n = Int64.Parse(Console.ReadLine());
+         Console.WriteLine("Input e:");
+         e = Int64.Parse(Console.ReadLine());
+         Console.WriteLine("Input number for encode:");
+         M = Int64.Parse(Console.ReadLine());
+         try
+         {
+            var C = rsa.Encode(M, new List<Int64>() { n, e });
+            Console.WriteLine("Encoded number:");
+            Console.WriteLine(C.ToString());
+         }
+         catch (Exception err)
+         {
+            Console.WriteLine(err);
+         }
+      }
+
+      static void Decode(Rsa rsa)
+      {
+         Int64 d, e, C;
+         Console.WriteLine("Input Private key");
+         Console.WriteLine("Input d:");
+         d = Int64.Parse(Console.ReadLine());
+         Console.WriteLine("Input e:");
+         e = Int64.Parse(Console.ReadLine());
+         Console.WriteLine("Input number for decode:");
+         C = Int64.Parse(Console.ReadLine());
+         try
+         {
+            var M = rsa.Decode(C, new List<Int64>() { d, e });
+            Console.WriteLine("Encoded number:");
+            Console.WriteLine(C.ToString());
+         }
+         catch (Exception err)
+         {
+            Console.WriteLine(err);
+         }
+      }
+
+      static void Main(string[] args)
+      {
+         Help();
+         string command = "";
+         var rsa = new Rsa();
+         while(command != "quit")
+         {
+            Console.Write(">");
+            command = Console.ReadLine();
+            switch(command)
+            {
+               case "gen": GenKeys(rsa);  break;
+               case "enc": Encode(rsa); break;
+               case "dec": Decode(rsa); break;
+               case "help": Help(); break;
+               case "quit": break;
+               default: Console.WriteLine("Uncorrect command"); break;
+            }
          }
          
 
